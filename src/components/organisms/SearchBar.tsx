668b840cb4d7587aff128f08f77cd01a5db45a51
wato787 <wato787@icloud.com>
2023-11-11 11:49:45 +0900

    loading

--- src/components/organisms/SearchBar.tsx
+++ src/components/organisms/SearchBar.tsx
@@ -3,20 +3,34 @@ import type { ReactElement } from "react";
 import * as React from "react";
 
 import SearchInput from "../molucules/SearchInput";
+import { LinearProgress } from "@mui/material";
 
 interface Props {
   isSearchInitialized: boolean;
+  loading?: boolean;
 }
 
 const SearchBar = (props: Props): ReactElement => {
   return (
-    <div className="border-gray-200 mb-3 flex flex-col border-b-2 pb-3">
-      <div className=" flex space-x-3  pb-3">
-        <SearchInput isSearchInitialized={props.isSearchInitialized} />
-        <div className={classNames("text-gray-300 opacity-80")}></div>
+    <>
+      <div
+        className={classNames(
+          " mb-3 flex flex-col  pb-3",
+          !props.loading && "border-gray-200 border-b-2",
+        )}
+      >
+        <div className=" flex space-x-3  pb-3">
+          <SearchInput isSearchInitialized={props.isSearchInitialized} />
+          <div className={classNames("text-gray-300 opacity-80")}></div>
+        </div>
+        {/* <SearchFilterChipsBar /> */}
       </div>
-      {/* <SearchFilterChipsBar /> */}
-    </div>
+      {props.loading && (
+        <div className="-mt-4">
+          <LinearProgress />
+        </div>
+      )}
+    </>
   );
 };
 
