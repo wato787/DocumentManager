dd772448fc67b65f08528aea5b43d15ec2c953f6
wato787 <wato787@icloud.com>
2023-11-13 19:42:25 +0900

    ログインページ修正

--- src/pages/auth/signin.tsx
+++ src/pages/auth/signin.tsx
@@ -1,24 +1,21 @@
 import { SignInResponse, signIn } from "next-auth/react";
 import Image from "next/image";
 import React from "react";
+import CommonButton from "~/components/atoms/CommonButton";
 
 const Signin = () => {
   return (
     <div className=" grid h-screen  items-center justify-center">
-      <div className="">
+      <div className="flex flex-col items-center">
         <Image width={400} height={600} src="/logo.png" alt="Document Logo" />
-        <div className="">
-          <div className=" justify-end">
-            <button
-              onClick={(): Promise<SignInResponse | undefined> =>
-                signIn("google", { callbackUrl: "/" })
-              }
-              className="btn btn-primary"
-            >
-              ログイン
-            </button>
-          </div>
-        </div>
+
+        <CommonButton
+          onClick={(): Promise<SignInResponse | undefined> =>
+            signIn("google", { callbackUrl: "/" })
+          }
+        >
+          ログイン
+        </CommonButton>
       </div>
     </div>
   );
