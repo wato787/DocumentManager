9d589a1fee757c8f393ca9d1699ca1a0e29e8916
wato787 <wato787@icloud.com>
2023-12-29 21:17:30 +0900

    ログイン修正

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -72,7 +72,7 @@ export const getServerSideProps: GetServerSideProps = async (context) => {
   if (!session) {
     return {
       redirect: {
-        destination: "/auth/signin",
+        destination: "/",
         permanent: false,
       },
     };
