7b1b001e2eb56a3a947da3e1e309e4f74bf1044d
wato787 <wato787@icloud.com>
2024-01-03 18:55:24 +0900

    signin関数切り分け

--- src/pages/auth/signin.tsx
+++ src/pages/auth/signin.tsx
@@ -4,18 +4,15 @@ import React from "react";
 import CommonButton from "~/components/atoms/CommonButton";
 
 const Signin = () => {
+  const handleSignin = async (): Promise<SignInResponse | undefined> => {
+    return signIn("google", { callbackUrl: "/" });
+  };
   return (
     <div className=" grid h-screen  items-center justify-center">
       <div className="flex flex-col items-center">
         <Image width={400} height={600} src="/logo.png" alt="Document Logo" />
 
-        <CommonButton
-          onClick={(): Promise<SignInResponse | undefined> =>
-            signIn("google", { callbackUrl: "/" })
-          }
-        >
-          ログイン
-        </CommonButton>
+        <CommonButton onClick={handleSignin}>ログイン</CommonButton>
       </div>
     </div>
   );
