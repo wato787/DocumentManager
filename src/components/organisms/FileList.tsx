f4243b8aefbf725909cf223675d9559a2a362647
wato787 <wato787@icloud.com>
2024-01-09 16:02:55 +0900

    検索機能　追加

--- src/components/organisms/FileList.tsx
+++ src/components/organisms/FileList.tsx
@@ -1,27 +1,31 @@
 import { File } from "@prisma/client";
-import React, { ReactElement } from "react";
+import React, { ReactElement, useMemo } from "react";
 
 import PdfContent from "~/components/molucules/PdfContent";
+import { useSearchKeywordStore } from "~/zustand/store/searchKeywordStore";
 
 interface Props {
   files?: File[];
 }
 
 const FileList = (props: Props): ReactElement => {
+  const keyword = useSearchKeywordStore((state) => state.keyword);
+
+  const filteredFiles = useMemo(() => {
+    if (!keyword || !props.files) return props.files;
+    return props.files.filter((file) => file.name.includes(keyword));
+  }, [keyword, props.files]);
+
   return (
     <div className="flex w-full flex-wrap gap-4 pt-4">
-      {props.files && (
-        <>
-          {props.files.map((file, index) => (
-            <PdfContent
-              jpgUrl={file.jpgUrl}
-              pdfUrl={file.pdfUrl}
-              title={file.name}
-              key={index}
-            />
-          ))}
-        </>
-      )}
+      {filteredFiles?.map((file, index) => (
+        <PdfContent
+          jpgUrl={file.jpgUrl}
+          pdfUrl={file.pdfUrl}
+          title={file.name}
+          key={index}
+        />
+      ))}
     </div>
   );
 };
